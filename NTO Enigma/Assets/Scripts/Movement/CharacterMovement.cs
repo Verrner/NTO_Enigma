94893ad4a6502588f61b8e00601d5d6c0adb7337
Verrner <ohteimmm@gmail.com>
2025-11-24 22:06:16 +0300

    Camera twitching

--- NTO Enigma/Assets/Scripts/Movement/CharacterMovement.cs
+++ NTO Enigma/Assets/Scripts/Movement/CharacterMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace NTO.Movement
@@ -8,43 +7,73 @@ namespace NTO.Movement
     {
         [Header("Movement"), SerializeField] private bool canMove = true;
         [SerializeField, Min(0)] private float speed;
-        
-        [Header("Running"), SerializeField] private bool canRun = true;
-        [SerializeField] private KeyCode runningKey = KeyCode.LeftShift;
-        [SerializeField, Min(1)] private float runningSpeedMultiplier;
 
         [Header("Camera Rotation"), SerializeField] private new Transform camera;
         [SerializeField] private bool canRotate = true;
         [SerializeField, Min(0)] private float sensitivity;
         [SerializeField, Min(0)] private float smoothing;
         [SerializeField] private bool lockCursor = true;
+
+        [Header("Camera Twitch"), SerializeField] private bool canTwitch = true;
+        [SerializeField] private AnimationCurve twitchCurve;
+        [SerializeField] private float twitchSpeed;
+        [SerializeField] private float twitchAmplitude;
         
         private Rigidbody _rigidbody;
+        
         private Vector2 _velocity;
         private Vector2 _frameVelocity;
+
+        private float _twitchTime;
+        private float _baseCameraHeight;
+        
+        public bool Moving { get; private set; }
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
             
             CheckCursorLocking();
+
+            _baseCameraHeight = camera.localPosition.y;
         }
 
         private void Update()
         {
             UpdateMovement();
+            UpdateCameraTwitch();
             UpdateCameraRotation();
         }
 
         private void UpdateMovement()
         {
-            var currentSpeed = canMove ? speed * (canRun && Input.GetKey(runningKey) ? runningSpeedMultiplier : 1) : 0;
-            var targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * currentSpeed * Time.deltaTime;
+            if (!canMove) return;
+            
+            var targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
             _rigidbody.linearVelocity = _rigidbody.rotation * new Vector3(targetVelocity.x, _rigidbody.linearVelocity.y, targetVelocity.y);
+
+            if (targetVelocity.magnitude != 0 == Moving) return;
+            
+            _twitchTime = 0;
+            Moving = targetVelocity.magnitude != 0;
+            camera.localPosition = new Vector3(camera.localPosition.x, _baseCameraHeight, camera.localPosition.z);
+        }
+
+        private void UpdateCameraTwitch()
+        {
+            if (!Moving || !canTwitch || twitchCurve.length < 2) return;
+            
+            _twitchTime = (_twitchTime + Time.deltaTime * twitchSpeed) % twitchCurve.keys[twitchCurve.length - 1].time;
+            
+            var twitch = twitchCurve.Evaluate(_twitchTime) * twitchAmplitude;
+
+            camera.localPosition = new Vector3(camera.localPosition.x, _baseCameraHeight + twitch, camera.localPosition.z);
         }
 
         private void UpdateCameraRotation()
         {
+            if (!canRotate) return;
+            
             var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
             var rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity * Time.deltaTime);
             _frameVelocity = Vector2.Lerp(_frameVelocity, rawFrameVelocity, 1 / smoothing);
