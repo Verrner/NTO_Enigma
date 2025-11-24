ddef18db2816c19ffc56871a91eba0c837b479a4
Verrner <ohteimmm@gmail.com>
2025-11-24 21:43:38 +0300

    Camera rotation + Textures fix

--- NTO Enigma/Assets/Scripts/Movement/CharacterMovement.cs
+++ NTO Enigma/Assets/Scripts/Movement/CharacterMovement.cs
@@ -13,53 +13,52 @@ namespace NTO.Movement
         [SerializeField] private KeyCode runningKey = KeyCode.LeftShift;
         [SerializeField, Min(1)] private float runningSpeedMultiplier;
 
-        public bool CanMove
-        {
-            get => canMove;
-            set => canMove = value;
-        }
-        
-        public bool CanRun
-        {
-            get => canRun;
-            set => canRun = value;
-        }
-        
-        public KeyCode RunningKey
-        {
-            get => runningKey;
-            set => runningKey = value;
-        }
-        
-        public float Speed
-        {
-            get => speed;
-            set => speed = Mathf.Max(0, value);
-        }
-        
-        public float RunningSpeedMultiplier
-        {
-            get => runningSpeedMultiplier;
-            set => runningSpeedMultiplier = Mathf.Max(1, value);
-        }
+        [Header("Camera Rotation"), SerializeField] private new Transform camera;
+        [SerializeField] private bool canRotate = true;
+        [SerializeField, Min(0)] private float sensitivity;
+        [SerializeField, Min(0)] private float smoothing;
+        [SerializeField] private bool lockCursor = true;
         
         private Rigidbody _rigidbody;
+        private Vector2 _velocity;
+        private Vector2 _frameVelocity;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            
+            CheckCursorLocking();
         }
 
         private void Update()
         {
             UpdateMovement();
+            UpdateCameraRotation();
         }
 
         private void UpdateMovement()
         {
-            var currentSpeed = CanMove ? speed * (CanRun && Input.GetKey(RunningKey) ? runningSpeedMultiplier : 1) : 0;
+            var currentSpeed = canMove ? speed * (canRun && Input.GetKey(runningKey) ? runningSpeedMultiplier : 1) : 0;
             var targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * currentSpeed * Time.deltaTime;
             _rigidbody.linearVelocity = _rigidbody.rotation * new Vector3(targetVelocity.x, _rigidbody.linearVelocity.y, targetVelocity.y);
+        }
+
+        private void UpdateCameraRotation()
+        {
+            var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            var rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity * Time.deltaTime);
+            _frameVelocity = Vector2.Lerp(_frameVelocity, rawFrameVelocity, 1 / smoothing);
+            _velocity += _frameVelocity;
+            _velocity.y = Mathf.Clamp(_velocity.y, -90, 90);
+            
+            camera.localRotation = Quaternion.AngleAxis(-_velocity.y, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(_velocity.x, Vector3.up);
+        }
+
+        private void CheckCursorLocking()
+        {
+            Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !lockCursor;
         }
     }
 }
